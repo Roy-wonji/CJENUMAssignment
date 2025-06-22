9c64ea7987cb6737a3f0fac85f2358e5d1194be7
Roy <shuwj81@daum.net>
2025-06-22 15:59:36 +0900

    ➕[add]:  프로젝트 생성

---
+++ CJEnmAssignment/CJEnmAssignment/CJEnmAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  CJEnmAssignmentApp.swift
+//  CJEnmAssignment
+//
+//  Created by Wonji Suh  on 6/22/25.
+//
+
+import SwiftUI
+
+@main
+struct CJEnmAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CJEnmAssignment/CJEnmAssignmentTests/CJEnmAssignmentTests.swift
@@ -0,0 +1,17 @@
+//
+//  CJEnmAssignmentTests.swift
+//  CJEnmAssignmentTests
+//
+//  Created by Wonji Suh  on 6/22/25.
+//
+
+import Testing
+@testable import CJEnmAssignment
+
+struct CJEnmAssignmentTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
